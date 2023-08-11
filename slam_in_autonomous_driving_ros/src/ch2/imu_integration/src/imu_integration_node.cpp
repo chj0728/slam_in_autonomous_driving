c9b6f81a45922755c7544ca0397dbd005af73c4b
cao-haojie <997650637@qq.com>
2023-08-11 23:38:43 +0800

    添加ch2的ros包

---
+++ slam_in_autonomous_driving_ros/src/ch2/imu_integration/src/imu_integration_node.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file imu_integration_node.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-08-11
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <imu_integration/imu_integration.h>
+
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "imu_integration_node");
+
+    Vec3d gravity(0, 0, -9.8);  // 重力方向
+    Vec3d init_bg(00.000224886, -7.61038e-05, -0.000742259);
+    Vec3d init_ba(-0.165205, 0.0926887, 0.0058049);
+
+    std::string file_path="/home/caohaojie/SLAM/BOOKS/slam_in_autonomous_driving_ros/src/data/state.txt";
+
+    IMUIntegration imu_integration(gravity, init_bg, init_ba, file_path);
+
+    ros::spin();
+
+    return (0);
+
+}
+
+
+
---
+++ slam_in_autonomous_driving_ros/src/scripts/plot_ch3_state.py
@@ -0,0 +1,62 @@
+# coding=UTF-8
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# 格式：t x y z qx qy qz qw vx vy vz
+if __name__ == '__main__':
+
+    # path = sys.argv[1]
+    path = "/home/caohaojie/SLAM/BOOKS/slam_in_autonomous_driving_ros/src/data/state.txt"
+    path_data = np.loadtxt(path)
+    plt.rcParams['figure.figsize'] = (16.0, 12.0)
+
+    # 轨迹
+    plt.subplot(221)
+    plt.scatter(path_data[:, 1], path_data[:, 2], s=2)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.grid()
+    plt.title('2D trajectory')
+
+    # 姿态
+    plt.subplot(222)
+    plt.plot(path_data[:, 0], path_data[:, 4], 'r')
+    plt.plot(path_data[:, 0], path_data[:, 5], 'g')
+    plt.plot(path_data[:, 0], path_data[:, 6], 'b')
+    plt.plot(path_data[:, 0], path_data[:, 7], 'k')
+    plt.title('q')
+    plt.legend(['qw', 'qx', 'qy', 'qz'])
+
+    # 速度
+    plt.subplot(224)
+    plt.plot(path_data[:, 0], path_data[:, 8], 'r')
+    plt.plot(path_data[:, 0], path_data[:, 9], 'g')
+    plt.plot(path_data[:, 0], path_data[:, 10], 'b')
+    plt.title('v')
+    plt.legend(['vx', 'vy', 'vz'])
+
+    # plt.show()        
+    
+    
+    # creat 3D trajector
+
+    # 方法1
+    #plt.figure()
+    #ax = plt.axes(projection = '3d')
+
+    # 方法2
+    ax = plt.subplot(223, projection='3d')
+    # ax = plt.figure().gca(projection='3d')
+
+    ax.scatter3D(path_data[:, 1], path_data[:, 2], path_data[:, 3] ,s=2, c="b")
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_title('3D trajector')
+    plt.legend(['position'],loc="best")#注释
+
+    plt.show()
+
+    exit(1)
